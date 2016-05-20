9fd31b46415a547b9925b4a9f64a51b1a1348798
Quan Nguyen <qnguyen32@csc.com>
2016-05-20 15:18:14 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,6 +14,8 @@ function showParameters() {
 
 function onMessage(e) {
     if (e.data && e.data.indexOf && e.data.indexOf("ANCILECSH") >= 0) {
+        var txt1 = document.getElementById("text1");
+        txt1.innerText = "Received " + e.data;
         e.source.postMessage("This is a message from github!", "https://jsbin.com");
     }
 }
